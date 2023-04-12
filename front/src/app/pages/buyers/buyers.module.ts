e51ad6bac7016e98269b061225847023c732cca4
Antonio Romano <aromanomacias@gmail.com>
2023-04-12 15:18:26 -0600

    adding inventory component

--- front/src/app/pages/buyers/buyers.module.ts
+++ front/src/app/pages/buyers/buyers.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 import { BuyersComponent } from './buyers.component';
-import { NgSelectModule } from '@ng-select/ng-select';
 
 const routes: Routes = [
   {
--- front/src/app/pages/inventory/inventory.module.ts
+++ front/src/app/pages/inventory/inventory.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
 
+import { InventoryComponent } from './inventory.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: InventoryComponent
+  }
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [InventoryComponent],
   imports: [
-    CommonModule
+    FormsModule,
+    CommonModule,
+    NgSelectModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class InventoryModule { }
