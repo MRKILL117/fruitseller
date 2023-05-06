3ed80618c11741864acb370056cc0daf38597ec5
Antonio Romano <aromanomacias@gmail.com>
2023-05-06 01:01:02 -0600

    order model fixes

--- back/server/models/order.js
+++ back/server/models/order.js
@@ -16,7 +16,7 @@ module.exports = function(Order) {
         const userId = ctx.accessToken.userId;
         Order.find({
             where: {adminId: userId, deleted: false},
-            order: 'date ASC'
+            order: 'date DESC'
         }, (err, orders) => {
             if(err) return callback(err);
 
@@ -28,20 +28,7 @@ module.exports = function(Order) {
         Order.upsert(order, (err, orderUpdated) => {
             if(err) return callback(err);
             
-            Order.findById(orderUpdated.id, {include: 'products'}, (err, orderUpdated) => {
-                if(err) return callback(err);
-
-                let productsToUnbind = orderUpdated.products().filter(prod => !Order.products.find(prod2 => prod2.id == prod.id));
-                Order.app.models.Product.updateAll({id: {inq: productsToUnbind.map(prod => prod.id)}}, {orderId: null}, (err, updated) => {
-                    if(err) return callback(err);
-                    
-                    Order.app.models.Product.updateAll({id: {inq: Order.products.map(prod => prod.id)}}, {orderId: orderUpdated.id}, (err, updated) => {
-                        if(err) return callback(err);
-    
-                        return callback(null, orderUpdated);
-                    });
-                });
-            });
+            return callback(null, orderUpdated);
         });
     }
 
