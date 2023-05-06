2853de8ae55c1c882eacd445c3abcfc9f1d280ea
Antonio Romano <aromanomacias@gmail.com>
2023-05-05 23:10:37 -0600

    create sell-order works

--- front/src/app/pages/create-sell-order/create-sell-order.component.ts
+++ front/src/app/pages/create-sell-order/create-sell-order.component.ts
@@ -12,6 +12,7 @@ import { environment } from 'src/environments/environment';
 })
 export class CreateSellOrderComponent implements OnInit {
 
+  IVA: number = 0.16;
   clients: Array<any> = [];
   selectedClient: any = null;
   measurementTypes: Array<any> = [];
@@ -21,9 +22,9 @@ export class CreateSellOrderComponent implements OnInit {
     product: null,
     quantity: null,
     weight: null,
-    tax: null,
-    price: null,
-    total: null,
+    tax: 0,
+    price: 0,
+    total: 0,
   }
   orderDate: moment.Moment = moment().tz(environment.timezone);
   loading: any = {
@@ -78,25 +79,19 @@ export class CreateSellOrderComponent implements OnInit {
   }
 
   GetProducts() {
-    this.loading.getting = true;
     this.http.Get(`Products/WithPriceHistory/FilteredBy/Text/*/StartDate/${this.orderDate.format('YYYY-MM-DD')}/EndDate/${this.orderDate.format('YYYY-MM-DD')}`).subscribe((products: any) => {
       this.products = products;
-      this.loading.getting = false;
     }, err => {
       console.error("Error getting products", err);
-      this.loading.getting = false;
     });
   }
 
   GetClients() {
-    this.loading.getting = true;
     let endpoint = `/Clients`;
     this.http.Get(endpoint).subscribe((clients: any) => {
       this.clients = clients;
-      this.loading.getting = false;
     }, err => {
       console.error("Error getting clients", err);
-      this.loading.getting = false;
     });
   }
 
@@ -106,20 +101,29 @@ export class CreateSellOrderComponent implements OnInit {
   RemoveItem(idx: number) {
     if(this.orderItems.length > 1) this.orderItems.splice(idx, 1);
   }
-
-  OnProductSelected(product: any, item: any) {
-    item.tax = product.tax;
-    item.price = product.prices.length ? product.prices[0] : item.price;
-    item.total = this.GetItemTotal(item);
-  }
-
+  
   OnWeightOrQuantityChange(item: any) {
-    item.total = this.GetItemTotal(item);
+    if(!!item.product) {
+      const productPrice = item.product.prices.length ? item.product.prices[0].salePrice : item.price;
+      let productQuantity = 0;
+      switch (item.product.salesMeasurementType.abrev) {
+        case 'kg': productQuantity = !!item.weight ? item.weight : 0; break;
+        case 'pz': productQuantity = !!item.quantity ? item.quantity : 0; break;
+        default: productQuantity = !!item.weight ? item.weight : 0; break;
+      }
+      if(!Number.isNaN(Number(productQuantity))) {
+        item.price = Number(productQuantity) * (productPrice + (productPrice*Number(this.selectedClient.utilityPercentage)/100));
+        item.tax = item.price * this.IVA;
+        item.total = item.price + item.tax;
+      }
+    }
   }
 
   SaveOrder() {
-    if(!this.ValidateOrderData()) {
-      this.toast.ShowDefaultWarning(`La orden no es valida`);
+    let errorMessage = this.ValidateOrderData();
+    if(!!errorMessage) {
+      this.toast.ShowDefaultWarning(errorMessage, `La orden no es valida`);
+      return;
     }
 
     let order = {
@@ -132,24 +136,24 @@ export class CreateSellOrderComponent implements OnInit {
     }
     this.http.Post(`Orders`, {order}).subscribe(newOrder => {
       this.toast.ShowDefaultSuccess(`Orden creada exitosamente`);
+      this.nav.GoToRoleRoute('sell-orders');
     }, err => {
       console.error("Error creating order", err);
     });
   }
 
   ValidateOrderData() {
-    if(!this.selectedClient) return false;
-    return true;
-  }
-
-  GetItemTotal(item: any): string | number {
-    let total = 0;
-    if(!!item && !!item.weight && !!item.product && !!this.selectedClient) {
-      total += item.price;
-      total += (item.price*item.tax);
-      total += (total * this.selectedClient.utilityPersentage / 100);
-    }
-    return total;
+    let errorMessage = null;
+    if(!this.selectedClient) return `No ha seleccionado cliente`;
+    this.orderItems.forEach((item, idx): any => {
+      if(!item.product) return errorMessage = `La fila ${idx+1} no tiene producto seleccionado`;
+      switch (item.product.salesMeasurementType.abrev) {
+        case 'kg': if(!item.weight || Number.isNaN(item.weight)) return errorMessage = `El producto ${item.product.name} no tiene cantidad de Kg valida`; break;
+        case 'pz': if(!item.quantity || Number.isNaN(item.quantity)) return errorMessage = `El producto ${item.product.name} no tiene cantidad de Pz valida`; break;
+        default: if(!item.weight || Number.isNaN(item.weight)) return errorMessage = `El producto ${item.product.name} no tiene cantidad de Kg valida`; break;
+      }
+    });
+    return errorMessage;
   }
 
 }
