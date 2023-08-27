c7e86467cb563de456bc6260d64b7efee3201b98
Antonio Romano <aromanomacias@gmail.com>
2023-08-26 23:32:08 -0600

    refactor

--- back/server/services/pdf.service.js
+++ back/server/services/pdf.service.js
@@ -39,7 +39,8 @@ class PdfService {
             `Guillermo Lúa / Humberto Lúa\n`,
             `Cel. 3331323279\n`,
             `Tel. 23.05.01.35\n`,
-            `Mail. ventas_hlua@hotmail.com\n`,
+            `Mail.\n`,
+            `ventas_hlua@hotmail.com\n`,
             `ventas_glua@gmail.com\n`,
         ];
         currentX += 115 + 5;
@@ -47,7 +48,7 @@ class PdfService {
             x: currentX,
             y: pageHeight - 40,
             maxWidth: 200,
-            lineHeight: 20,
+            lineHeight: 18,
             size: 12
         });
         texts = [
@@ -67,16 +68,82 @@ class PdfService {
             y: pageHeight - 40,
             maxWidth: 130,
             lineHeight: 15,
-            size: 12
+            size: 12,
         });
         // ------------------------- End pdf Header ------------------------- //
+        // ------------------------- Table of items ------------------------- //
+        let separations = [85, 60, 140, 95, 85, 105];
+        let headers = [`Unidad`, `Cant.`, `Producto`, `Precio`, `Importe`, `Observaciones`];
+        let cellHeight = 20;
+        currentX = 20;
+        currentY = 200;
+
+        separations.forEach((separation, idx) => {
+            const text = headers[idx];
+            page.drawRectangle({
+                x: currentX,
+                y: pageHeight - currentY,
+                width: separation,
+                height: -cellHeight,
+                borderWidth: 2,
+                opacity: 0
+            });
+            page.drawText(text, {
+                x: currentX + 5,
+                y: pageHeight - (currentY + cellHeight - 5),
+                maxWidth: separation - 10,
+                size: 14
+            });
+            currentX += separation;
+        });
+        
+        currentY += cellHeight + 5;
+        order.items.forEach(item => {
+            currentX = 20;
+            let content = [
+                `${item.product.salesMeasurementType.abrev}`,
+                `${item.weight}`,
+                item.product.name,
+                `$${Number(item.total).toFixed(2)}`,
+                `$${Number(item.price).toFixed(2)}`,
+                `${order.comments ? order.comments : ''}`,
+            ];
+            separations.forEach((separation, idx) => {
+                const text = content[idx];
+                page.drawRectangle({
+                    x: currentX,
+                    y: pageHeight - currentY,
+                    width: separation,
+                    height: -cellHeight,
+                    borderWidth: 1,
+                    opacity: 0
+                });
+                page.drawText(text, {
+                    x: currentX + 5,
+                    y: pageHeight - (currentY + cellHeight - 6),
+                    maxWidth: separation - 10,
+                    size: 12
+                });
+                currentX += separation;
+            });
+            currentY += cellHeight;
+        });
+        // ------------------------- End table of items ------------------------- //
+        // ------------------------- Footer ------------------------- //
+        page.drawLine({
+            start: {x: 20, y: 200},
+            end: {x: pageWidth - 20, y: 200},
+            thickness: 2
+        })
+        // ------------------------- End Footer ------------------------- //
         
 
         // Save PDF in server
         const name = randomUUID();
         const pdfBytes = await this.pdf.save();
         await fs.writeFile(`server/files/orders-resumes/${name}.pdf`, pdfBytes);
-        return {name, file: pdfBytes};
+        const parcialRoute = `/Files/orders-resumes/download/${name}.pdf`;
+        return {path: parcialRoute, file: pdfBytes};
     } catch (err) {
         throw err;
     }
