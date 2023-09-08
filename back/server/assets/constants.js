2a67e7816f63f98628abeebdde897a9065a4b654
Antonio Romano <aromanomacias@gmail.com>
2023-09-08 15:30:27 -0600

    updating host utl in back constants

--- back/server/assets/constants.js
+++ back/server/assets/constants.js
@@ -1,5 +1,5 @@
 'use strict';
 
-module.exports.hostURL = "http://localhost:4201/#";
+module.exports.hostURL = "http://fruitseller.romanoserver.com/#";
 module.exports.timezone = "America/Mexico_City";
 module.exports.dateFormat = "YYYY-MM-DD";
